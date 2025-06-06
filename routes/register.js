c7285f6268ec751b6d17706940470952a9db0f2d
Tibor Hajszan <thajszan@t-online.hu>
2025-06-06 15:07:47 +0200

    Updated /register route handler to check for existing user and store hash passwords.

--- routes/register.js
+++ routes/register.js
@@ -6,6 +6,7 @@
 
 const router = require("express").Router();
 const fileSystem = require("fs");
+const bcrypt = require("bcrypt-nodejs");
 
 // post /register ######################################################################################################
 
@@ -13,12 +14,21 @@ router.post("/", (request, response) => {
 
   // no database file > error ------------------------------------------------------------------------------------------
   if (!fileSystem.existsSync("users.json")) {
-    response.status(404).send(`<p>${request.originalUrl} database not found</p>`);
+    response.status(404).send(`<p>${request.originalUrl} : database not found</p>`);
+    return;
   };
   
-  // reading file > parsing content ------------------------------------------------------------------------------------
+  // reading database file > parsing content ---------------------------------------------------------------------------
   const fileContent = fileSystem.readFileSync("users.json", "utf-8");
   const users = JSON.parse(fileContent);
+
+  // existing user > error ---------------------------------------------------------------------------------------------
+  for (const user of Object.values(users)) {
+    if (user.email === request.body.email) {
+      response.status(400).send(`<p>${request.originalUrl} : user already exists</p>`);
+      return;
+    };
+  };
 
   // creating user id --------------------------------------------------------------------------------------------------
   let userId = 1;
@@ -26,20 +36,21 @@ router.post("/", (request, response) => {
     userId = Math.max(...Object.keys(users)) + 1;
   };
 
-  // registering new user ----------------------------------------------------------------------------------------------
+  // creating new user record ------------------------------------------------------------------------------------------
   users[userId] = {
     id: userId,
     name: request.body.name,
     email: request.body.email,
-    password: request.body.password,
+    password: bcrypt.hashSync(request.body.password),
     detects: 0,
     lastLogin: new Date(),
   };
   
-  // writing file > returning new user ---------------------------------------------------------------------------------
+  // writing database file > returning new user record -----------------------------------------------------------------
   fileSystem.writeFileSync("users.json", JSON.stringify(users));
   response.json(users[userId]);
 
+  return;
 });
 
 // exports #############################################################################################################
